7e0c3b4bb0e3e6aaa256869c113f4f8472d9da62
bonjugi <vjava114@gmail.com>
2019-07-28 17:42:34 +0900

    리커션 - log.debug -> sout 으로 변경

--- src/main/java/dev/bonjugi/algorithmstudy/recursion/Recursion.java
+++ src/main/java/dev/bonjugi/algorithmstudy/recursion/Recursion.java
@@ -1,18 +1,13 @@
 package dev.bonjugi.algorithmstudy.recursion;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class Recursion {
-
 
 	static void recursionPrint(String input){
 
 		// base case
 		if (input.length() <= 0) {
 		} else {
-			log.debug(input);
-
+			System.out.println(input);
 			recursionPrint(input.substring(1));
 		}
 
